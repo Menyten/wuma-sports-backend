6baf152ee1d5837a0eba2e4c0fe4018e6abc7d0f
Joel <joel.pedersen97@gmail.com>
2019-06-10 17:27:30 +0200

    changed port to 3001

--- api/routes.js
+++ api/routes.js
@@ -5,4 +5,7 @@ router.post('/api/verifyToken', (req, res) => {
   res.send('Another head hangs lowly!!!!')
 });
 
+router.get('/api/message', (req, res) => {
+  res.send('Kebab till middag!!')
+});
 module.exports = router;
