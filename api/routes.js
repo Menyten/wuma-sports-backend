26c9a99944a15e04e5973b024600aad6ba95a334
Joel <joel.pedersen97@gmail.com>
2019-06-12 18:39:02 +0200

    added route for userDetails

--- api/routes.js
+++ api/routes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const verify = require('../helpers/verifyToken');
 
+router.get('/api/user', (req, res) => {
+  console.log(req.headers);
+  res.json({ msg: 'Something' });
+});
+
 router.post('/api/verifyToken', (req, res) => {
   verify(req.body.token).catch(err => console.log(err));
-  res.json({ msg: 'Verified' })
+  res.json({ msg: 'Verified' });
 });
 
 module.exports = router;
