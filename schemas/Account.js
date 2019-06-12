682fb050dcb42cafa902a2676851ca65070c6934
Pontus Nilsson <p0nta_@hotmail.com>
2019-06-12 17:24:44 +0200

    merge conflict fix

--- schemas/Account.js
+++ schemas/Account.js
@@ -8,8 +8,4 @@ const AccountSchema = new Schema({
   accountLevel: Number,
 })
 
-<<<<<<< HEAD
 module.exports = mongoose.model('Account', AccountSchema)
-=======
-module.exports = mongoose.model('Account', AccountSchema)
->>>>>>> 40a972b2f33cf895cd9c4c3f86fcb0db660d44c9
